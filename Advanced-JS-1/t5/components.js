fb49b7278b865172e281d28ca2843d7031049468
Khai Chan <strubo.gt@gmail.com>
2023-09-06 19:53:45 +0300

    updated

--- Advanced-JS-1/t5/components.js
+++ Advanced-JS-1/t5/components.js
@@ -15,9 +15,26 @@ const restaurantRow = restaurant => {
 
 const restaurantModal = (restaurant, menu) => {
   const {name, address, city, postalCode, phone} = restuarant;
-
+  let html = menuHtml += `
+  <tr>
+    <td>${course.name}</td>
+    <td>${course.diets || ' - '}</td>
+    <td>${course.price || ' - '}</td>
+  </tr>
+  `;
   for(const course of menu.courses) {
      const {name, diets, price} = course;
+     html +=
+      <tr>
+        <td>${name}</td>
+        <td>${diets ?? ' - '}</td>
+        <td>${price ?? ' - '}</td>
+      </tr>
+     ;
   }
-}
-export {restaurantRow};
+  html += 'table';
+  return html
+};
+
+
+export {restaurantRow, restaurantModal};
